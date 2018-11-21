812a8031a4c3e2104db0ff597dc3d18dc7181866
Charles Wilson <charles@meandyouis.us>
2018-11-21 13:57:24 -0700

    Added Nano

--- brew.sh
+++ brew.sh
@@ -64,6 +64,7 @@ grep -q "$LINE" ~/.extra || echo "$LINE" >> ~/.extra
 
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
+brew install nano --override-system-nano
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
